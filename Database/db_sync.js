b0eb14be39bb4968754a1f2b813e03bbbe0e33ae
Hamza Bham <92208535+AliHamzaBham@users.noreply.github.com>
2024-06-01 18:01:44 +0500

    QUIKS-9: Added create user in db_sync

--- Database/db_sync.js
+++ Database/db_sync.js
@@ -1,5 +1,14 @@
 // Currently made a book table using the query
 
+// CREATE TABLE Users (
+//     id INT NOT NULL UNIQUE AUTO_INCREMENT PRIMARY KEY,             
+//     name VARCHAR(255) NOT NULL,          
+//     email VARCHAR(255) UNIQUE NOT NULL,  
+//     age INT,                             
+//     profile_pic VARCHAR(255),            
+//     bio TEXT                            
+// )
+
 // CREATE TABLE Books (
 //     id INT NOT NULL UNIQUE AUTO_INCREMENT PRIMARY KEY,
 //     title VARCHAR(255) UNIQUE,
@@ -9,7 +18,7 @@
 //     content TEXT,
 //     audio_data LONGBLOB,
 //     author_id INT,
-//     FOREIGN KEY (author_id) REFERENCES Users(user_id)
+//     FOREIGN KEY (author_id) REFERENCES Users(id)
 // );
 
 
